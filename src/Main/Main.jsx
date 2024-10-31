2454caaa555f1ae66dded8ec46cde43380382278
abwilson <abwilson2002@gmail.com>
2024-10-31 13:53:23 -0600

    updated deploy files

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -46,7 +46,33 @@ export function Main(props) {
 export function Main() {
     return (
       <main className='container-fluid bg-secondary text-center'>
-        <div>Main page to find books here</div>
+        <h6>User's Username</h6>
+        <table>
+            <tr>
+                <th>Book Picture</th>
+                <th>Title Of Book</th>
+                <th>Active Users</th>
+                <th>New Comments?</th>
+            </tr>
+            <tr id="booklist">
+                <td><img src="/book_image.jpg" alt="Example Book" height="100" width="100"/></td>
+                <td>
+                  <a href="Book">Example Book Title</a>
+                </td>
+                <td>8</td>
+                <td>
+                  <li>
+                    <input type="checkbox" id="checkbox1" name="varCheckbox" value="checkbox1" checked/>
+                  </li>
+                </td>
+            </tr>
+            <tr id="booklist">
+                <td><img src="/book_image.jpg" alt="Example Book" height="100" width="100"/></td>
+                <td><a href="Book">Example Book Title 2</a></td>
+                <td>15</td>
+                <td><li><input type="checkbox" id="checkbox2" name="varCheckbox" value="checkbox2"/></li></td>
+            </tr>
+        </table>
       </main>
     );
   }
