efe4deb21c03df4594cc1b16b12a5004290b9e24
abwilson <abwilson2002@gmail.com>
2024-10-31 13:11:59 -0600

    updated About page, it's practically done!

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React from "react";
+import "./About.css";
 /*
-import "./About.css"
+
 export function About(props) {
     const [imageUrl, setImageUrl] = React.useState('././public/Bookmarked_Theories_Logo.png.jpg');
 
@@ -12,20 +13,21 @@ export function About(props) {
 
 
     return (
-        <main>
-            <img src={imageUrl} alt="Logo" height="200" width="150"/>
-            <p>Welcome to Bookmarked Theories! This is a site dedicated to allowing anyone who wishes to discuss a book to have that opportunity without the chance for someone to spoil it for you.</p>
-            <p>What to do: Create a profile, choose a book to discuss, enter the page number you are on, and chat!</p>
-            <h2>That's all there is!</h2>
-            <p>Now go enjoy discussion your favorite books without concern for spoilers!</p>
-        </main>
+
     );
 }*/
 
 export function About() {
     return (
       <main className='container-fluid bg-secondary text-center'>
-        <div>This page has the about</div>
-      </main>
+        <div id="picture" className="picture-box">
+            <img src="/Bookmarked_Theories_Logo.png.jpg" alt="Logo" height="200" width="150"/>
+        </div>
+        
+        <p>Welcome to Bookmarked Theories! This is a site dedicated to allowing anyone who wishes to discuss a book to have that opportunity without the chance for someone to spoil it for you.</p>
+        <p>What to do: Create a profile, choose a book to discuss, enter the page number you are on, and chat!</p>
+        <h2>That's all there is!</h2>
+        <p>Now go enjoy discussion your favorite books without concern for spoilers!</p>
+    </main>
     );
 }
--- src/app.jsx
+++ src/app.jsx
@@ -62,10 +62,7 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-function NotFound() {
-  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-} */
+*/
 
 export default function App() {
   return (
