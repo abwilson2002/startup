9de94e2aac66d3bf7975d2d55a01d965072368af
abwilson <abwilson2002@gmail.com>
2024-11-08 11:44:16 -0700

    Did a lot of editing to get my Book to work correctly

--- src/Book/Book.jsx
+++ src/Book/Book.jsx
@@ -1,64 +1,27 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Book.css";
-
-/*
-
-export function About(props) {
-    const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=')
-
-
-    React.useEffect(() => {
-        setImageUrl('book_image.jpg');
-    }, []);
-
-
-    return (
-            <main>
-                <section>
-                    <img src={imageUrl} alt="Book image" height="400" width="300"/>
-                    <table>
-                        <tr>
-                            <th>Chat</th>
-                        </tr>
-                        <tr>
-                            <td class="messages"id="selfReport">Example message from user</td>
-                        </tr>
-                        <tr>
-                            <p><td class="messages" id="otherReport">Example reply message from other users</td></p>
-                        </tr>
-                    </table>
-            </section>
-            <section id="userInput">
-                <div id="userInput">
-                    <input class="buttonInput" type="text" placeholder="Type your response here"/>
-                    <button class="buttonChat" type="Submit">Chat</button>
-                </div>
-            </section>
-        </main>
-    );
-}*/
 
 export function Book() {
     return (
-      <main className='container-fluid bg-secondary text-center'>
-        <section>
-            <img src="/book_image.jpg" className="fixed-left" alt="Book image" height="400" width="300"/>
-            <table className='container-fluid fixed-right'>
+      <main>
+        <section className="main-container">
+            <img className="book-image" src="/book_image.jpg" alt="Book image"/>
+            <table className="chat-table">
                 <tr>
                     <th>Chat</th>
                 </tr>
                 <tr>
-                    <td className="text-right" class="messages"id="selfReport">Example message from user</td>                    
+                    <td className="messages"id="selfReport">Example message from user</td>                    
                 </tr>
                 <tr>
-                    <p><td className='text-left' class="messages" id="otherReport">Example reply message from other users</td></p>
+                    <p><td className="messages" id="otherReport">Example reply message from other users</td></p>
                 </tr>
             </table>
         </section>
         <section id="userInput">
-            <input class="buttonInput" type="text" placeholder="Type your response here"/>
-            <button class="buttonChat" type="Submit">Chat</button>
+            <input className="buttonInput" type="text" placeholder="Type your response here"/>
+            <button className="buttonChat" type="Submit">Chat</button>
         </section>
       </main>
     );
